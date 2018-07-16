40792dbd6c2565ec3683a35a569c0a168f0eb61a
Nguyen Hoang Gia <gia01020304@gmail.com>
2018-07-16 11:22:07 +0700

    fetch_4

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -26,7 +26,7 @@ namespace WindowsFormsApplication1
 
 
             MessageBox.Show("fetch1");
-              MessageBox.Show("fetch2");
+              MessageBox.Show("fetch3");
         }
     }
 }
